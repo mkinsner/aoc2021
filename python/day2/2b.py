6c9be7ff7e15f26616c39f9678a368955bd95680
Mike Kinsner <mike.kinsner@gmail.com>
2021-12-02 22:46:55 -0400

    Clean up day 2 solutions

--- python/day2/2b.py
+++ python/day2/2b.py
@@ -5,8 +5,8 @@ with open("input.txt") as f:
 split_lines = [x.split() for x in lines]
 
 horiz = list(map(lambda x: int(x[1]) if x[0][0] == 'f' else -int(x[1]) if x[0][0] == 'b' else 0, split_lines))
-aim_deltas = list(map(lambda x: int(x[1]) if x[0][0] == 'd' else -int(x[1]) if x[0][0] == 'u' else 0, split_lines))
-aim = list(accumulate(aim_deltas))
+aim_deltas = map(lambda x: int(x[1]) if x[0][0] == 'd' else -int(x[1]) if x[0][0] == 'u' else 0, split_lines)
+aim = accumulate(aim_deltas)
 vert = [x*y for x,y in zip(horiz,aim)]
 
 print(sum(horiz)*sum(vert))
