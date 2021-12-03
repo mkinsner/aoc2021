8756993a888702f867cd687b23cd0a5af6173772
Mike Kinsner <mike.kinsner@gmail.com>
2021-12-02 21:35:55 -0400

    Day 2 solution

---
+++ python/day2/2a.py
@@ -0,0 +1,8 @@
+with open("input.txt") as f:
+  lines = f.readlines()
+split_lines = [x.split() for x in lines]
+
+horiz = list(map(lambda x: int(x[1]) if x[0][0] == 'f' else -int(x[1]) if x[0][0] == 'b' else 0, split_lines))
+vert = list(map(lambda x: int(x[1]) if x[0][0] == 'd' else -int(x[1]) if x[0][0] == 'u' else 0, split_lines))
+
+print(sum(horiz)*sum(vert))
---
+++ python/day2/2b.py
@@ -0,0 +1,12 @@
+from itertools import accumulate
+
+with open("input.txt") as f:
+  lines = f.readlines()
+split_lines = [x.split() for x in lines]
+
+horiz = list(map(lambda x: int(x[1]) if x[0][0] == 'f' else -int(x[1]) if x[0][0] == 'b' else 0, split_lines))
+aim_deltas = list(map(lambda x: int(x[1]) if x[0][0] == 'd' else -int(x[1]) if x[0][0] == 'u' else 0, split_lines))
+aim = list(accumulate(aim_deltas))
+vert = [x*y for x,y in zip(horiz,aim)]
+
+print(sum(horiz)*sum(vert))
