fd8a78a8d9e56ef76021d27f85357b077f9a82a2
Mike Kinsner <mike.kinsner@gmail.com>
2021-12-02 22:46:08 -0400

    Clean up day 1 solutions

--- python/day1/1a.py
+++ python/day1/1a.py
@@ -4,8 +4,7 @@ with open('input.txt') as f:
   lines = f.readlines()
 
 ints = list(map(int, lines))
-
-diffs = list(map(operator.sub, ints[1:], ints[:-1]))
+diffs = map(operator.sub, ints[1:], ints[:-1])
 
 positives = sum(x>0 for x in diffs)
 print(positives)
--- python/day1/1b.py
+++ python/day1/1b.py
@@ -2,13 +2,9 @@ with open("input.txt") as f:
   lines = f.readlines()
 
 ints = list(map(int, lines))
-
 sums = [ints[i]+ints[i+1]+ints[i+2] for i in range(len(ints)-2)]
 
-def mysub(a,b):
-  return a-b
-
-diffs = list(map(mysub, sums[1:], sums[:-1]))
+diffs = list(map(lambda a,b : a-b, sums[1:], sums[:-1]))
 
 increased = sum(x>0 for x in diffs)
 print(increased)
