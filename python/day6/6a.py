3e55ab6bf772bd194c29a525ca0a81b98af74f46
Mike Kinsner <mike.kinsner@gmail.com>
2021-12-06 20:47:59 -0400

    Day 6 solutions

---
+++ python/day6/6a.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+with open("input.txt") as f:
+  lines = f.readlines()
+in_data = lines[0].strip().split(",")
+
+ages = np.zeros(9, int)  # index is age, content is number fish of that age
+for f in in_data: ages[int(f)] += 1 
+
+count = 0
+for day in range(80):
+  new_spawning = ages[0]
+  ages[:-1] = ages[1:]
+  ages[8] = new_spawning
+  ages[6] += new_spawning
+
+print(sum(ages))
